d37228cb54a1468b80b13f1970ed1e5db89c9a8f
Jay <lakemove@gmail.com>
2013-07-17 14:23:44 +0800

    coding the matrix : hw0 completed

--- matrix/hw0/hw0.py
+++ matrix/hw0/hw0.py
@@ -5,55 +5,75 @@
 
 
 ## Problem 1
-def myFilter(L, num): pass
+def myFilter(L, num): return [x for x in L if x%num != 0]
 
 
 
 ## Problem 2
-def myLists(L): pass
+def myLists(L): return [list(range(1,x+1)) for x in L]
 
 
 
 ## Problem 3
-def myFunctionComposition(f, g): pass
+def myFunctionComposition(f, g): return {x:g[x] for x in f}
 
 
 ## Problem 4
 # Please only enter your numerical solution.
 
-complex_addition_a = ... 
-complex_addition_b = ...
-complex_addition_c = ...
-complex_addition_d = ...
+complex_addition_a = 5 + 3i
+complex_addition_b = i
+complex_addition_c = -1 + .001i
+complex_addition_d = .001 + 9i
 
 
 
 ## Problem 5
-GF2_sum_1 = ...
-GF2_sum_2 = ...
-GF2_sum_3 = ...
+GF2_sum_1 = 1
+GF2_sum_2 = 0
+GF2_sum_3 = 0
 
 
 ## Problem 6
-def mySum(L): pass
+def mySum(L): 
+    current = 0
+    for x in L :
+    	current += x
+    return current
 
 
 
 ## Problem 7
-def myProduct(L): pass
+def myProduct(L): 
+    current = 1
+    for x in L :
+    	current *= x
+    return current
 
 
 
 ## Problem 8
-def myMin(L): pass
+def myMin(L): 
+    current = L[0]
+    for x in L :
+    	current = x if x < current else current
+    return current
 
 
 
 ## Problem 9
-def myConcat(L): pass
+def myConcat(L): 
+    current = ''
+    for x in L :
+    	current += x
+    return current
 
 
 
 ## Problem 10
-def myUnion(L): pass
+def myUnion(L): 
+    current = set()
+    for x in L :
+    	current = current | x
+    return current;
 
